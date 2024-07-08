1260a4a1707db686bed6a18a24059aa001655af7
ArtZ <snickgem@gmail.com>
2024-07-08 22:26:24 +0300

    hero, middle blocks

--- src/js/partials/swiper-init.js
+++ src/js/partials/swiper-init.js
@@ -1,39 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const catalogCarousel = document.querySelectorAll(".config__slider");
+	const solutionsCarousel = document.querySelectorAll(".solutions__slider");
 
-	if (catalogCarousel.length > 0) {
-		catalogCarousel.forEach(elem => {
-			const catalogNext = elem.querySelector(".config__nav .config__btn--next");
-			const catalogPrev = elem.querySelector(".config__nav .config__btn--prev");
+	if (solutionsCarousel.length > 0) {
+		solutionsCarousel.forEach(elem => {
+			const solutionsNext = elem.previousElementSibling.querySelector(".swiper-btn-next");
+			const solutionsPrev = elem.previousElementSibling.querySelector(".swiper-btn-prev");
 
 			const slider = new Swiper(elem, {
-				slidesPerView: 1.23,
-				spaceBetween: 16,
-				centeredSlides: true,
-				initialSlide: 1,
+				slidesPerView: 1,
+				spaceBetween: 10,
 				loop: true,
 				navigation: {
-					nextEl: catalogNext,
-					prevEl: catalogPrev,
-				},
-				pagination: {
-					el: ".config__pagination",
-					clickable: true,
-				},
-				on: {
-					init: function (swiper) {
-						swiper.el.querySelector(".config__nav").style.width = swiper.slides[0].swiperSlideSize + "px";
-					},
-				},
-				breakpoints: {
-					768: {
-						slidesPerView: 1.3,
-						spaceBetween: 120,
-					},
-					1440: {
-						slidesPerView: 1.711,
-						spaceBetween: 100,
-					}
+					nextEl: solutionsNext,
+					prevEl: solutionsPrev,
 				}
 			});
 		});
