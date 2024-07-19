6a833ceba8b566582e7b587d9685da2cd9ab125f
Zhukov <azhukov@cnews.ru>
2024-07-19 18:10:55 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
 /* Libs */
-//= ./../../node_modules/jquery/dist/jquery.min.js
 //= ./../../node_modules/swiper/swiper-bundle.min.js
-//= ./libs/select2.min.js
-//= ./libs/parallax.js
-//= ./libs/odometer.min.js
-//= ./libs/hystmodal.min.js
 
 /* Common */
 //= ./partials/main.js
